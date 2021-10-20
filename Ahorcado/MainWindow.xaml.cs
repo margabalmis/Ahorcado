3496d7ef4deacb9184048927b45004b1234bc74e
Marga Arroyo <margarita.arroyo@iesdoctorbalmis.com>
2021-10-20 12:07:41 +0200

    UFF

--- Ahorcado/MainWindow.xaml.cs
+++ Ahorcado/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
+
 namespace Ahorcado
 {
     /// <summary>
@@ -20,9 +21,57 @@ namespace Ahorcado
     /// </summary>
     public partial class MainWindow : Window
     {
+        string[] abc = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K","L", "M", "N",
+                                        "Ñ", "O", "P", "Q", "R", "S", "T", "U","V", "W","X", "Y", "Z" };
+        List<string> palabrasAdivinar = new List<string>() { "INFIERNO", "ÑU"};
+        
         public MainWindow()
         {
             InitializeComponent();
+            CrearBotonesLetras();
+            ObtenerPalabra();
         }
+
+        private void ObtenerPalabra()
+        {
+            List<int> numPalabraUtilizada = new List<int>();
+
+            Random rnd = new Random();
+            int numPalabra = rnd.Next(0, palabrasAdivinar.Count);
+            numPalabraUtilizada.Add(numPalabra);
+
+            if () { 
+            
+            
+            }
+            
+
+            
+        }
+
+        public void CrearBotonesLetras() {
+
+
+            for (int i = 0; i < abc.Length; i++)
+            {
+                Button b = new Button()
+                {
+                    Tag = abc[i],
+                    Style = (Style)this.Resources["abcButtons"]
+                };
+                
+                Viewbox view = new Viewbox();
+                Label letra = new Label();
+                letra.Content = abc[i];
+                view.Child = letra;
+                b.Content = view;
+
+                letrasUniformGrid.Children.Add(b);
+            }
+
+            
+        
+        }
+
     }
 }
