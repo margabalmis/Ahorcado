edd02af22f8ae11a534bd8b30b2cb17028a6f70e
Marga Arroyo <margarita.arroyo@iesdoctorbalmis.com>
2021-10-21 12:53:20 +0200

    clase jueves

--- Ahorcado/MainWindow.xaml.cs
+++ Ahorcado/MainWindow.xaml.cs
@@ -21,32 +21,54 @@ namespace Ahorcado
     /// </summary>
     public partial class MainWindow : Window
     {
-        string[] abc = new string[] { "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K","L", "M", "N",
-                                        "Ñ", "O", "P", "Q", "R", "S", "T", "U","V", "W","X", "Y", "Z" };
+        char[] abc = new char[] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K','L', 'M', 'N',
+                                   'Ñ', 'O', 'P', 'Q', 'R', 'S', 'T', 'U','V', 'W','X', 'Y', 'Z' };
         List<string> palabrasAdivinar = new List<string>() { "INFIERNO", "ÑU"};
-        
+        string palabraSeleccionada;
+
+
         public MainWindow()
         {
             InitializeComponent();
             CrearBotonesLetras();
             ObtenerPalabra();
+            GerenarGuiones();
+        }
+
+        private void GerenarGuiones()
+        {
+            char[] letras = palabraSeleccionada.ToCharArray();
+            int numGuiones = palabraSeleccionada.Length;
+            WrapPanel guiones = new WrapPanel();
+
+            for ( int i = 0; i< numGuiones; i++) {
+
+                Viewbox viewGuiones = new Viewbox();
+                Border border = new Border();
+                Label guion = new Label();
+                TextBlock letra = new TextBlock();
+
+                letra.MinWidth = 25;
+                letra.Text = letras[i].ToString();
+                letra.Visibility = Visibility.Hidden;
+                
+                border.BorderBrush = Brushes.Black;
+                border.BorderThickness = new Thickness(0, 0, 0, 2);
+
+                guion.Content = letra;
+                border.Child = guion;
+                viewGuiones.Child = border;
+                guiones.Children.Add(viewGuiones);
+                
+            }
+            palabraYGuiones.Content = guiones;
         }
 
         private void ObtenerPalabra()
         {
-            List<int> numPalabraUtilizada = new List<int>();
-
             Random rnd = new Random();
             int numPalabra = rnd.Next(0, palabrasAdivinar.Count);
-            numPalabraUtilizada.Add(numPalabra);
-
-            if () { 
-            
-            
-            }
-            
-
-            
+            palabraSeleccionada = palabrasAdivinar[numPalabra];
         }
 
         public void CrearBotonesLetras() {
@@ -61,7 +83,9 @@ namespace Ahorcado
                 };
                 
                 Viewbox view = new Viewbox();
+
                 Label letra = new Label();
+
                 letra.Content = abc[i];
                 view.Child = letra;
                 b.Content = view;
